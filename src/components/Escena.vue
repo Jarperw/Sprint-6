c6043be48937b146797ec834d1c54d4be485fcf8
jarperw <you@example.com>
2023-02-25 13:37:45 +0100

    ejercicio-3

--- src/components/Escena.vue
+++ src/components/Escena.vue
@@ -12,3 +12,27 @@ export default {
   props: ["historia"]
 }
 </script>
+
+<style scoped>
+p {
+  text-align: center;
+  padding: 1em;
+  margin: 1.2em;
+  border: 0.13em solid #000;
+  border-radius: 3em;
+}
+
+@media (max-width: 992px) {
+  p {
+    font-size: smaller;
+    border: 0.15em solid #000;
+  }
+}
+
+@media (max-width: 768px) {
+  p {
+    font-size: 0.73em;
+    border: 0.172em solid #000;
+  }
+}
+</style>
